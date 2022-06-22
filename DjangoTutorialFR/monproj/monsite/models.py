a04f9e7fe4498c3ee59beec2a54ee8ce6f3db129
Ivan Savov <ivan.savov@gmail.com>
2022-06-22 13:31:28 -0400

    Ajouter le modèle Book

--- DjangoTutorialFR/monproj/monsite/models.py
+++ DjangoTutorialFR/monproj/monsite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    # id
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
